a295dc66f227f22bbadf682228cd6d1dbfdda50b
keras <rkstager@gmail.com>
2022-04-20 08:43:31 -0700

    test push

--- minitrain.sh
+++ minitrain.sh
@@ -1,2 +1,3 @@
+echo run small image training 
 python train.py --outdir=~/training-runs --cfg=stylegan2 --data=~/data/img_align_celeb_png \
     --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug
